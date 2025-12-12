682a5bce8c310b6befd2bd87d0504927813e194b
Teronn Sutton <teronnsutton@gmail.com>
2025-12-12 13:15:47 -0700

    Refactor car model and year property access

--- server/database/app.js
+++ server/database/app.js
@@ -106,8 +106,9 @@ app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
 		"purchase": data['purchase'],
 		"purchase_date": data['purchase_date'],
 		"car_make": data['car_make'],
-		"car_model": data['car_model'],
-		"car_year": data['car_year'],
+	  	 car_model: data.car_model,
+		 car_year: data.car_year,
+		
 	});
 
   try {
